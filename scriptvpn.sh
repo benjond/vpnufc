a5464115aa0750650e7cfa1542578f4fc75beb34
Benjamin Jond <benjamin.jond@edu.univ-fcomte.fr>
2025-04-03 13:25:44 +0200

    v

--- scriptvpn.sh
+++ scriptvpn.sh
@@ -41,7 +41,7 @@ function configurer_vpn() {
     # Supprimer toutes les connexions VPN existantes (actives ou non)
     vpn_connections="$(sudo nmcli --terse --fields NAME,TYPE connection show | grep vpn | cut -d: -f1)"
     for vpn in $vpn_connections; do
-        sudo nmcli connection delete "$vpn"
+        sudo nmcli connection delete "$(echo "$vpn" | xargs)"
     done
     echo "Veuillez entrer vos identifiants de connexion"
     read -p "Nom d'utilisateur: " username
